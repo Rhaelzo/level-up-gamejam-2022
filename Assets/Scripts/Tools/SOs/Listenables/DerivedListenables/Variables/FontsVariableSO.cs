da9a25119f88923d015ad44e2a10bc178d9ce880
Rhaelzo <rhaelzo@outlook.pt>
2022-06-24 11:42:33 +0100

    Added fonts variable SO to hold used fonts

---
+++ Assets/Scripts/Tools/SOs/Listenables/DerivedListenables/Variables/FontsVariableSO.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+using TMPro;
+
+[CreateAssetMenu(fileName = "New font variable SO"
+    , menuName = "LevelUpGJ/Variables/Font variable SO", order = 0)]
+public class FontsVariableSO : VariableSO<TMP_FontAsset[]> { }
