68132c72f177b4f78cdd2f62927c68b03aeb5c14
Rhaelzo <rhaelzo@outlook.pt>
2022-06-25 15:36:54 +0100

    Added documentation to CharacterType

--- Assets/Scripts/Enums/CharacterType.cs
+++ Assets/Scripts/Enums/CharacterType.cs
@@ -1,5 +1,10 @@
 using System;
 
+/// <summary>
+/// Enum that defines a character type
+/// <para/>
+/// A character can be defined as a player, enemy or none type
+/// </summary>
 [Serializable]
 public enum CharacterType
 {
