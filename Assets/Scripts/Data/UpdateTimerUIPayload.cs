f7c020013fe2efd8bdc84d09c0f177d590dd5843
Rhaelzo <rhaelzo@outlook.pt>
2022-06-25 14:56:17 +0100

    Added documentation to UpdateTimerUIPayload

--- Assets/Scripts/Data/UpdateTimerUIPayload.cs
+++ Assets/Scripts/Data/UpdateTimerUIPayload.cs
@@ -1,8 +1,22 @@
+/// <summary>
+/// Content payload <see cref="MessageContentPayload"/> used in message
+/// communication between controllers and UI controllables related to the
+/// timer UI, such as current time and time per round
+/// </summary>
 public class UpdateTimerUIPayload : MessageContentPayload
 {
     public float CurrentTime { get; private set; }
     public float TimePerRound { get; private set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UpdateTimerUIPayload"/> class.
+    /// </summary>
+    /// <param name="currentTime">
+    /// Current time
+    /// </param>
+    /// <param name="timePerRound">
+    /// Time per round
+    /// </param>
     public UpdateTimerUIPayload(float currentTime, float timePerRound)
     {
         CurrentTime = currentTime;
