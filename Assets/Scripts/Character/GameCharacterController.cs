6d11027a71f81948e2e8deb9beebb1e4415c0a3c
Rhaelzo <rhaelzo@outlook.pt>
2022-06-25 13:31:59 +0100

    Added documentation to GameCharacterController

--- Assets/Scripts/Character/GameCharacterController.cs
+++ Assets/Scripts/Character/GameCharacterController.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
 
+/// <summary>
+/// Controller class (<see cref="GenericController{T,V}"/>) responsible for controlling
+/// everything character related that implement the <see cref="TControllable"/>, such
+/// as the <see cref="Character"/> class.
+/// <para/>
+/// It's also used as a communication intermediary between the "model" and the "view" or UI
+/// classes
+/// </summary>
 public class GameCharacterController : GenericController<TControllable, CharacterEvent>
 {
+    /// <summary>
+    /// Event callback for when a word is finished being written, 
+    /// sending a message to the corresponding messageables
+    /// </summary>
+    /// <param name="eventData">
+    /// Event data corresponding to a data array with the word
+    /// string value, the points value and the target type
+    /// </param>
     public virtual void Event_OnWordFinished(object eventData)
     {
         if (eventData is object[] dataArray)
