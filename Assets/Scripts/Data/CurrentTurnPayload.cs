525dd63f1d5e7da96435b9a0e096b601d25ad0ee
Rhaelzo <rhaelzo@outlook.pt>
2022-06-25 14:55:14 +0100

    Added documentation to CurrentTurnPayload

--- Assets/Scripts/Data/CurrentTurnPayload.cs
+++ Assets/Scripts/Data/CurrentTurnPayload.cs
@@ -1,7 +1,18 @@
+/// <summary>
+/// Content payload <see cref="MessageContentPayload"/> used in message
+/// communication between controllers and controllables related to the
+/// current turn state
+/// </summary>
 public class CurrentTurnPayload : MessageContentPayload
 {
     public Turn Turn { get; private set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CurrentTurnPayload"/> class.
+    /// </summary>
+    /// <param name="turn">
+    /// Current turn state
+    /// </param>
     public CurrentTurnPayload(Turn turn)
     {
         Turn = turn;
