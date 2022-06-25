d808f5e6265e18d3cf73a524a377d4ab53382672
Rhaelzo <rhaelzo@outlook.pt>
2022-06-25 20:31:22 +0100

    Created WordInputUIEvent

---
+++ Assets/Scripts/Enums/WordInputUIEvent.cs
@@ -0,0 +1,11 @@
+using System;
+
+/// <summary>
+/// Enum with all of the word input related message events
+/// </summary>
+[Serializable]
+public enum WordInputUIEvent
+{
+    ClearInput,
+    TurnChanged
+}
