e325d75774b0b016f6e6677a24acabfb8359f267
Rhaelzo <rhaelzo@outlook.pt>
2022-06-25 15:37:52 +0100

    Added documentation to Turn

--- Assets/Scripts/Enums/Turn.cs
+++ Assets/Scripts/Enums/Turn.cs
@@ -1,5 +1,10 @@
 using System;
 
+/// <summary>
+/// Enum that defines in which turn the game currently is
+/// <para/>
+/// A turn can be defined as player's, enemy's, both and none turns
+/// </summary>
 [Serializable]
 public enum Turn
 {
