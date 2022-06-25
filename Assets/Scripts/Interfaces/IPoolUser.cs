2173003710cd333ce79e7d19b29bd7523969f3c5
Rhaelzo <rhaelzo@outlook.pt>
2022-06-25 23:56:23 +0100

    Added documentation to IPoolUser

--- Assets/Scripts/Interfaces/IPoolUser.cs
+++ Assets/Scripts/Interfaces/IPoolUser.cs
@@ -1,5 +1,22 @@
+/// <summary>
+/// Interface that describes an entity that can needs access to
+/// an object pooler
+/// </summary>
 public interface IPoolUser<T>
 {
+    /// <summary>
+    /// Loads a new object from the pool
+    /// </summary>
+    /// <returns>
+    /// Object from the pool
+    /// </returns>
     T LoadNewObjectFromPool();
+
+    /// <summary>
+    /// Returns an active object to the pool
+    /// </summary>
+    /// <param name="objectToReturn">
+    /// Object to be returned
+    /// </param>
     void ReturnObjectToPool(T objectToReturn);
 }
