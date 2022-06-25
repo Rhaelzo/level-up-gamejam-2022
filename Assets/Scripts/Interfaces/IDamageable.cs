9e41fd7a4c6299828c027c539619095fef3a5915
Rhaelzo <rhaelzo@outlook.pt>
2022-06-25 13:34:56 +0100

    Added documentation to IDamageable

--- Assets/Scripts/Interfaces/IDamageable.cs
+++ Assets/Scripts/Interfaces/IDamageable.cs
@@ -1,8 +1,24 @@
+/// <summary>
+/// Interface that describes an entity that can be damaged
+/// </summary>
 public interface IDamageable 
 {
     int MaxHealth { get; }
     int Health { get; }
 
+    /// <summary>
+    /// Increases health by given amount 
+    /// </summary>
+    /// <param name="amount">
+    /// Amount to increase health by
+    /// </param>
     void IncreaseHealth(int amount);
+
+    /// <summary>
+    /// Reduces health by given amount 
+    /// </summary>
+    /// <param name="amount">
+    /// Amount to reduce health by
+    /// </param>
     void ReduceHealth(int amount);
 }
